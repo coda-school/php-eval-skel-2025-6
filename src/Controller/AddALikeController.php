6617b29c92aa79cc3c8367858a49fcb37ad93bb2
Remy <remy.martinot@coda-student.school>
2026-02-05 18:49:06 +0100

    feat: ajout de la première fonction du controlleur addalike

--- src/Controller/AddALikeController.php
+++ src/Controller/AddALikeController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\ProfileXLike;
 use App\Entity\Ver;
+use App\Service\ProfileService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -14,8 +18,27 @@ final class AddALikeController extends AbstractController
     public function create(
         #[MapEntity(mapping: ['id' => 'id'])]
         Ver $ver,
+        Request $request,
+        ProfileService $profileService,
+        EntityManagerInterface $em
     ): Response
     {
-        return
+        $currentStatus = $request->request->get('liked');
+        $user = $this->getUser();
+        $profileUser = $profileService->getProfile($user->getId());
+
+        if($currentStatus === 'not-liked'){
+            $relation = new ProfileXLike();
+            $relation->setProfileId($profileUser);
+            $relation->setVerId($ver);
+            $em->persist($relation);
+            $em->flush();
+            $ver->setLikes($ver->getLikes() + 1);
+            $em->persist($ver);
+            $em->flush();
+        }
+        return $this->redirect('/ver/'.$ver->getId());
+
+
     }
 }
