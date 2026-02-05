dd62ea7b893620c123e531c50bc395e90d7eaaf2
Jrouget <rougetjoshua@gmail.com>
2026-02-05 23:17:44 +0100

    feat : ajouté la page de modification de profil

---
+++ src/Controller/Profile/GestionProfilController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller\Profile;
+
+use App\Form\ProfileType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class GestionProfilController extends AbstractController
+{
+    #[Route('/profile/edit', name: 'profile_edit')]
+    public function edit(Request $request, EntityManagerInterface $em): Response
+    {
+        $user = $this->getUser();
+
+        if (!$user) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        $form = $this->createForm(ProfileType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+            return $this->redirectToRoute('profile', ['id' => $user->getId()]);
+        }
+
+        return $this->render('profile/modifier.html.twig', [
+            'form' => $form,
+            'isUserLogged' => '_layouts/in_app.html.twig',
+        ]);
+    }
+}
