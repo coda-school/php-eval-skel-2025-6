59961cbafcf4593f32d279b1159c614ca8301240
Remy <remy.martinot@coda-student.school>
2026-02-05 19:33:55 +0100

    feat: fonctionnalité de like

--- src/Controller/AddALikeController.php
+++ src/Controller/AddALikeController.php
@@ -11,10 +11,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 final class AddALikeController extends AbstractController
 {
     #[Route('/like-adding/{id}', name: 'like-adding', methods: ['POST'])]
+    #[IsGranted('ROLE_USER')]
     public function create(
         #[MapEntity(mapping: ['id' => 'id'])]
         Ver $ver,
@@ -23,7 +25,7 @@ final class AddALikeController extends AbstractController
         EntityManagerInterface $em
     ): Response
     {
-        $currentStatus = $request->request->get('liked');
+        $currentStatus = $request->request->get('like');
         $user = $this->getUser();
         $profileUser = $profileService->getProfile($user->getId());
 
@@ -34,6 +36,16 @@ final class AddALikeController extends AbstractController
             $em->persist($relation);
             $em->flush();
             $ver->setLikes($ver->getLikes() + 1);
+            $em->persist($ver);
+            $em->flush();
+        }else{
+            $relationToDelete = $em->getRepository(ProfileXLike::class)->findBy([
+                'profile_id' => $profileUser->getId(),
+                'ver_id' => $ver->getId()
+            ]);
+            $em->remove($relationToDelete[0]);
+            $em->flush();
+            $ver->setLikes($ver->getLikes() - 1);
             $em->persist($ver);
             $em->flush();
         }
