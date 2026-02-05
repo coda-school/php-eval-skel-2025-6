f8fd9f823009ec2f56e5aba55f8ba30fabdbaa99
Remy <remy.martinot@coda-student.school>
2026-02-05 11:09:02 +0100

    feat: ajout des tendances lorsque l'on est connecté

--- src/Controller/Home/IndexController.php
+++ src/Controller/Home/IndexController.php
@@ -12,11 +12,19 @@ final class IndexController extends AbstractController
     #[Route('/', name: 'home', methods: ['GET'])]
     public function index(VerService $vers): Response
     {
+        $isUserLogged = false;
+        $path = '_layouts/standard.html.twig';
+        if($this->getUser()){
+            $isUserLogged = true;
+            $path = '_layouts/in_app.html.twig';
+        }
         $trendingVers = $vers->getTrendingVers();
 
         return $this->render('home/index/index.html.twig', [
             'controller_name' => 'Home/IndexController',
-            'trendingVers' => $trendingVers
+            'trendingVers' => $trendingVers,
+            'isUserLogged' => $isUserLogged,
+            'path' => $path
         ]);
     }
 }
